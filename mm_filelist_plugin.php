e77d14089d70d7f276a785d6eff3103f957cd863
MManifesto <MManifesto@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-23 16:14:48 +0000

    tagging version 0.3
    git-svn-id: https://plugins.svn.wordpress.org/mmm-file-list/tags/0.3@995512 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ mm_filelist_plugin.php
@@ -0,0 +1,125 @@
+<?php
+/*
+Plugin Name: Mmm Simple File List
+Plugin URI: http://www.mediamanifesto.com
+Description: Plugin to list files in a given directory using this shortcode [MMFileList folder="optional starting from base uploads path" format="li (html) or comma (txt)"" types="optional file-extension e.g. pdf,doc" class="optional css class for html list"]
+Version: 0.3
+Author: Adam Bissonnette
+Author URI: http://www.mediamanifesto.com
+*/
+
+class MM_FileList
+{
+    function MM_FileList()
+    {
+        add_shortcode( 'MMFileList', array(&$this, 'ListFiles') );
+    }
+    
+    function ListFiles($atts)
+    {   
+        extract( shortcode_atts( array(
+        'folder' => '',
+        'format' => 'li',
+        'types' => 'pdf,doc',
+        'class' => ''
+        ), $atts ) );
+        
+        $baseDir = wp_upload_dir(); //Base Upload Directory
+        $dir = $baseDir['path'] . $folder;
+        $outputDir = $baseDir['url'] . $folder;
+        
+        $typesToList = explode(",", $types);
+        $files = scandir($dir);
+        $list = array();
+        
+        foreach($files as $file)
+        {
+            $path_parts = pathinfo($file);
+
+            if (isset($path_parts['extension'])) //check for folders - don't list them
+            {
+                $extension = $path_parts['extension'];
+                
+                if($file != '.' && $file != '..' && in_array($extension, $typesToList))
+                {        
+                    if(!is_dir($dir.'/'.$file))
+                    {
+                        array_push($list, array("name" => $file, "url" => $outputDir . "/" . $file, "size" => $this->human_filesize(filesize($dir . '/' . $file))));
+                    } 
+                }
+            }
+        }
+        
+        $output = "";
+        
+        switch($format){
+            case 'table':
+                return $this->_MakeTabularLIst($list, $class);
+            break;
+            case 'comma':
+                $output = $this->_MakeCommaDelimitedList($list);
+            break;
+            case 'li':
+            default:
+                return $this->_MakeUnorderedList($list, $class);
+            break;
+        }
+        
+        return $output;
+    }
+
+    function _MakeCommaDelimitedList($list)
+    {
+        $formattedList = array_map(function($entry) { $listItemTemplate = "%s"; return sprintf($listItemTemplate, $entry["url"]); },$list);
+
+        return implode(",", $formattedList);
+    }
+
+    function _MakeUnorderedList($list, $class)
+    {
+        //These templates could be set as editable / saveable options
+        $listTemplate = '<ul class="' . $class . '">%s</ul>';
+        $listItemTemplate = '<li><a href="%s"><span class="filename">%s</span><span class="filesize"> (%s)</span></a></li>';
+        
+        $items = "";
+        
+        foreach ($list as $file => $fileatts) //in this case item == filename, value == path
+        {
+            $items .= sprintf($listItemTemplate, $fileatts["url"], $fileatts["name"], $fileatts["size"]);
+        }
+        
+        return sprintf($listTemplate, $items);
+    }
+
+    function _MakeTabularList($list, $class)
+    {
+        $listTemplate = '<table class="%s">%s%s</table>';
+        $listHeadingTemplate = '<tr><th class="filename">Filename / Link</th><th class="filesize">Size</th></tr>';
+        $listItemTemplate = '<tr><td class="filename"><a href="%s">%s</a></td><td class="filesize">%s</td></tr>';
+
+        $items = "";
+
+        foreach ($list as $filename => $fileatts) {
+            $items .= sprintf($listItemTemplate, $fileatts["url"], $fileatts["name"], $fileatts["size"]);
+        }
+
+        return sprintf($listTemplate, $class, $listHeadingTemplate, $items);
+    }
+
+    //Stolen from comments : http://php.net/manual/en/function.filesize.php thx Rommelsantor.com
+    function human_filesize($bytes, $decimals = 2) {
+      $sz = 'BKMGTP';
+      $factor = floor((strlen($bytes) - 1) / 3);
+      return sprintf("%.{$decimals}f", $bytes / pow(1024, $factor)) . @$sz[$factor];
+    }
+
+} // end class
+
+
+add_action( 'init', 'MM_FileList_Init', 5 );
+function MM_FileList_Init()
+{
+    global $MM_FileList;
+    $MM_FileList = new MM_FileList();
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,88 @@
+=== Mmm Simple File List ===
+Contributors: MManifesto
+Donate link: http://www.mediamanifesto.com/donate/
+Tags: File List, Shortcode
+Requires at least: 3.4
+Tested up to: 4.0
+Stable tag: 4.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Plugin to list files in a given directory using a basic shortcode.
+
+== Description ==
+
+This is a simple plugin to list files in a given directory using this shortcode: [MMFileList  /].
+
+**Parameters:**
+
+* **folder**: Relative to the base uploads directory of your wordpress install (e.g. siteurl.com/wp-content/uploads/ or siteurl.com/media).
+* **format**: Tabular (format="table") or Unordered list (format="li") or comma-delimited (format="comma")
+* **types**: Only list given file types (e.g. types="pdf,doc,txt")
+* **class**: Only used for the "li" format, applies a given class to the unordered list (e.g. class="mmm-list")
+
+**Output:**
+
+For all html (li) formats you can expect to see the following output wrapped in styleable containers:
+
+* Filename (linked to the File Url)
+* File Size
+
+At this point "comma" is the only available text output and it only outputs the url.
+
+
+**Usage Examples:**
+
+Let's say you're using the default WordPress Media settings so we can expect your uploads folder to be in /wp-content/uploads with this in mind the shortcode "folder" attribute will look in a directory relative to this.  So if we used the following:
+
+[MMFileList folder="/cats/" format="table" types="png" /]
+
+This will result in a tabular list of all .png files in the /wp-content/uploads/cats/ folder.  It's important to ensure that you add the first "/" in that folder attribute to ensure you don't end up with the system looks for a directory like /wp-content/uploadscats/.
+
+
+== Installation ==
+
+1. Download and install the plugin from WordPress dashboard. You can also upload the entire “MmmFileList” folder to the `/wp-content/plugins/` directory
+1. Activate the plugin through the ‘Plugins’ menu in WordPress
+
+
+== Frequently Asked Questions ==
+
+= Why should I use this plugin? =
+
+Say you have a folder on your webserver with 30 files you want to list but you don't want to tediously write out the html, load them as media to your WordPress site or edit your htaccess to allow directory listing.  This would be the ideal case to have a quick and dirty solution that handles updates to files without additional work on your part.
+
+= Are there other output formats available? =
+
+Not at this time.  If you want to request them via the forums here then I can have them added fairly quickly.
+
+= Why not have a settings page or upload functionality? =
+
+The idea behind this plugin is to be really simple and not mess with your site.  The plugin file itself is designed so that you could just copy / paste it into your functions.php and it would run without even needing to worry about a plugin.  If you are looking for a full featured file manager you should take a look at [File Away](https://wordpress.org/support/view/plugin-reviews/file-away).
+
+== Screenshots ==
+
+1. Sample of the "li" output used with a fairly large set of bylaws.
+
+== Changelog ==
+
+= 0.3 =
+* Added "table" output format
+* Added "filesize" to information that is output (this should automatically format to the nearest reasonable size B,K,M,G etc..)
+* Adjusted how the file array is built so it's more extensible
+* General Code Cleanup (naming changes, readibility prioritized over condensed & dehydrated code)
+
+= 0.2 =
+* Fixed a bug related to folders within the given path
+* Updated support docs and plugin description to show that folder is the base uploads directory and not the base directory.
+
+= 0.1 =
+* Initial release to WordPress.org
+
+== Upgrade Notice ==
+
+= 0.3 =
+Adds functionality and some code cleanup.
+
+= 0.2 =
+If you're having trouble with folders in your chosen directory then you should upgrade to fix that bug.
